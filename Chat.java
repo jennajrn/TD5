0943b9f91c790b150b928410c219b7cc5dcb8e97
Jenna Journo <jennajourno02@gmail.com>
2022-11-21 18:21:14 +0100

    Ajout fonctionnalité faireDuBruit pour les animaux implémentant l'interface Bruit

--- Chat.java
+++ Chat.java
@@ -1,9 +1,13 @@
 package TD5;
 
-public class Chat extends Animal{
+public class Chat extends Animal implements Bruit{
   @Override
   public void seDeplacer(int x, int y){
     super.seDeplacer(x,y);
     System.out.println("Miaou, je bouge");
+  }
+
+  public void faireDuBruit(){
+    System.out.println("Miaou");
   }
 }
--- Chien.java
+++ Chien.java
@@ -1,9 +1,13 @@
 package TD5;
 
-public class Chien extends Animal{
+public class Chien extends Animal implements Bruit{
   @Override
   public void seDeplacer(int x, int y){
     super.seDeplacer(x,y);
     System.out.println("Wouf, je bouge");
+  }
+
+  public void faireDuBruit(){
+    System.out.println("Wouf");
   }
 }
