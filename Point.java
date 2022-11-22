ab99224569fffb7f3899cf56682cb0a788a4a329
Shana2003 <118822187+Shana2003@users.noreply.github.com>
2022-11-22 14:18:06 +0100

    update3 Point.java

--- Point.java
+++ Point.java
@@ -1,8 +1,8 @@
 package TD5;
 
 public class Point{
-  private int abs=20;
-  private int ord=20;
+  private int abs=2;
+  private int ord=2;
 
   public void translation(int x, int y){
     abs = abs+x;
